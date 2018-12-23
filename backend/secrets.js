9bffd80ec1c329fa4a463f7ba2c1e2c17618a3b5
Thisuru <thisurudk@gmail.com>
2018-12-23 19:06:49 +0530

    added yarn.lock temporary to push heroku

--- backend/secrets.js
+++ backend/secrets.js
@@ -1,5 +1,7 @@
 const secrets = {
-    dbUri: "mongodb://localhost:27017/mern-comment-box"
+    // dbUri: "mongodb://localhost:27017/mern-comment-box"
+    dbUri: "mongodb://root:Thisuru@11@ds141674.mlab.com:41674/mern-comment-box"
+  
   };
   
   export const getSecret = key => secrets[key];
