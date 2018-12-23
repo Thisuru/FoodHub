f54a6521b02856518e6df6d32d5517b1c6b871a4
Thisuru <thisurudk@gmail.com>
2018-12-23 19:49:44 +0530

    escaped @ sign in pwd

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,9 @@ const router = express.Router();
 // set our port to either a predetermined port number if you have set it up, or 3001
 const API_PORT = process.env.API_PORT || 3001;
 
-mongoose.connect(getSecret('dbUri'));
+mongoose.connect(getSecret('dbUri'), { 
+  uri_decode_auth: true 
+  });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
