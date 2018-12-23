86c3cb9fa64fc28e3d18f3af1afbe2a85086f497
Thisuru <thisurudk@gmail.com>
2018-12-23 19:55:20 +0530

    changed the port syntax

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 const router = express.Router();
 
 // set our port to either a predetermined port number if you have set it up, or 3001
-const API_PORT = process.env.API_PORT || 3001;
+const API_PORT = process.env.PORT || 3001;
 
 mongoose.connect(getSecret('dbUri'), { 
   uri_decode_auth: true 
